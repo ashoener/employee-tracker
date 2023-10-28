98471e449cdfc2f68aaf23e47e772b42f073f32f
Andrew Shoener <a.b.shoener@gmail.com>
2023-10-27 20:34:15 -0500

    Start adding prompts Add View All Employees and View All Departments

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ import db from "./lib/db.js";
 import Department from "./lib/schemas/department.js";
 import Role from "./lib/schemas/role.js";
 import Employee from "./lib/schemas/employee.js";
+import BasePrompt from "./lib/promptsTypes/basePrompt.js";
+import ListPrompt from "./lib/promptsTypes/listPrompt.js";
 
 await db.sync({ force: false });
 console.log(`Connected to db ${process.env.DB_NAME}`);
+
+import mainPrompt from "./lib/prompts/mainPrompt.js";
+
+mainPrompt();
